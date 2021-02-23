c2e7a1d867d45491d9a0c77a44a772c323d23c80
Matteo Beltrame <bltmtt@gmail.com>
2021-02-23 20:18:57 +0100

    Namespace

--- Runtime/Scripts/Patterns/Event.cs
+++ Runtime/Scripts/Patterns/Event.cs
@@ -6,167 +6,170 @@
 
 using System;
 
-public class Event
+namespace GibFrame.Patterns
 {
-    private event Action Delegate;
-
-    public Event()
-    { }
-
-    public void Subscribe(Action Callback)
+    public class Event
     {
-        Delegate += Callback;
-    }
+        private event Action Delegate;
 
-    public void Unsubscribe(Action Callback)
-    {
-        Delegate -= Callback;
-    }
+        public Event()
+        { }
 
-    public void Broadcast()
-    {
-        Delegate?.Invoke();
-    }
-
-    public void Purge()
-    {
-        Delegate[] delegates = Delegate.GetInvocationList();
-        foreach (Delegate del in delegates)
+        public void Subscribe(Action Callback)
         {
-            Delegate -= (del as Action);
+            Delegate += Callback;
         }
-        Delegate = null;
-    }
-}
 
-public class Event<T>
-{
-    private event Action<T> Delegate;
-
-    public Event()
-    { }
-
-    public void Subscribe(Action<T> Callback)
-    {
-        Delegate += Callback;
-    }
-
-    public void Unsubscribe(Action<T> Callback)
-    {
-        Delegate -= Callback;
-    }
-
-    public void Broadcast(T arg)
-    {
-        Delegate?.Invoke(arg);
-    }
-
-    public void Purge()
-    {
-        Delegate[] delegates = Delegate.GetInvocationList();
-        foreach (Delegate del in delegates)
+        public void Unsubscribe(Action Callback)
         {
-            Delegate -= (del as Action<T>);
+            Delegate -= Callback;
         }
-        Delegate = null;
-    }
-}
 
-public class Event<T1, T2>
-{
-    private event Action<T1, T2> Delegate;
-
-    public Event()
-    { }
-
-    public void Subscribe(Action<T1, T2> Callback)
-    {
-        Delegate += Callback;
-    }
-
-    public void Unsubscribe(Action<T1, T2> Callback)
-    {
-        Delegate -= Callback;
-    }
-
-    public void Broadcast(T1 arg1, T2 arg2)
-    {
-        Delegate?.Invoke(arg1, arg2);
-    }
-
-    public void Purge()
-    {
-        Delegate[] delegates = Delegate.GetInvocationList();
-        foreach (Delegate del in delegates)
+        public void Broadcast()
         {
-            Delegate -= (del as Action<T1, T2>);
+            Delegate?.Invoke();
         }
-        Delegate = null;
-    }
-}
 
-public class Event<T1, T2, T3>
-{
-    private event Action<T1, T2, T3> Delegate;
-
-    public Event()
-    { }
-
-    public void Subscribe(Action<T1, T2, T3> Callback)
-    {
-        Delegate += Callback;
-    }
-
-    public void Unsubscribe(Action<T1, T2, T3> Callback)
-    {
-        Delegate -= Callback;
-    }
-
-    public void Broadcast(T1 arg1, T2 arg2, T3 arg3)
-    {
-        Delegate?.Invoke(arg1, arg2, arg3);
-    }
-
-    public void Purge()
-    {
-        Delegate[] delegates = Delegate.GetInvocationList();
-        foreach (Delegate del in delegates)
+        public void Purge()
         {
-            Delegate -= (del as Action<T1, T2, T3>);
+            Delegate[] delegates = Delegate.GetInvocationList();
+            foreach (Delegate del in delegates)
+            {
+                Delegate -= (del as Action);
+            }
+            Delegate = null;
         }
-        Delegate = null;
-    }
-}
-
-public class Event<T1, T2, T3, T4>
-{
-    private event Action<T1, T2, T3, T4> Delegate;
-
-    public Event()
-    { }
-
-    public void Subscribe(Action<T1, T2, T3, T4> Callback)
-    {
-        Delegate += Callback;
     }
 
-    public void Unsubscribe(Action<T1, T2, T3, T4> Callback)
+    public class Event<T>
     {
-        Delegate -= Callback;
-    }
+        private event Action<T> Delegate;
 
-    public void Broadcast(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
-    {
-        Delegate?.Invoke(arg1, arg2, arg3, arg4);
-    }
+        public Event()
+        { }
 
-    public void Purge()
-    {
-        Delegate[] delegates = Delegate.GetInvocationList();
-        foreach (Delegate del in delegates)
+        public void Subscribe(Action<T> Callback)
         {
-            Delegate -= (del as Action<T1, T2, T3, T4>);
+            Delegate += Callback;
         }
-        Delegate = null;
+
+        public void Unsubscribe(Action<T> Callback)
+        {
+            Delegate -= Callback;
+        }
+
+        public void Broadcast(T arg)
+        {
+            Delegate?.Invoke(arg);
+        }
+
+        public void Purge()
+        {
+            Delegate[] delegates = Delegate.GetInvocationList();
+            foreach (Delegate del in delegates)
+            {
+                Delegate -= (del as Action<T>);
+            }
+            Delegate = null;
+        }
+    }
+
+    public class Event<T1, T2>
+    {
+        private event Action<T1, T2> Delegate;
+
+        public Event()
+        { }
+
+        public void Subscribe(Action<T1, T2> Callback)
+        {
+            Delegate += Callback;
+        }
+
+        public void Unsubscribe(Action<T1, T2> Callback)
+        {
+            Delegate -= Callback;
+        }
+
+        public void Broadcast(T1 arg1, T2 arg2)
+        {
+            Delegate?.Invoke(arg1, arg2);
+        }
+
+        public void Purge()
+        {
+            Delegate[] delegates = Delegate.GetInvocationList();
+            foreach (Delegate del in delegates)
+            {
+                Delegate -= (del as Action<T1, T2>);
+            }
+            Delegate = null;
+        }
+    }
+
+    public class Event<T1, T2, T3>
+    {
+        private event Action<T1, T2, T3> Delegate;
+
+        public Event()
+        { }
+
+        public void Subscribe(Action<T1, T2, T3> Callback)
+        {
+            Delegate += Callback;
+        }
+
+        public void Unsubscribe(Action<T1, T2, T3> Callback)
+        {
+            Delegate -= Callback;
+        }
+
+        public void Broadcast(T1 arg1, T2 arg2, T3 arg3)
+        {
+            Delegate?.Invoke(arg1, arg2, arg3);
+        }
+
+        public void Purge()
+        {
+            Delegate[] delegates = Delegate.GetInvocationList();
+            foreach (Delegate del in delegates)
+            {
+                Delegate -= (del as Action<T1, T2, T3>);
+            }
+            Delegate = null;
+        }
+    }
+
+    public class Event<T1, T2, T3, T4>
+    {
+        private event Action<T1, T2, T3, T4> Delegate;
+
+        public Event()
+        { }
+
+        public void Subscribe(Action<T1, T2, T3, T4> Callback)
+        {
+            Delegate += Callback;
+        }
+
+        public void Unsubscribe(Action<T1, T2, T3, T4> Callback)
+        {
+            Delegate -= Callback;
+        }
+
+        public void Broadcast(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            Delegate?.Invoke(arg1, arg2, arg3, arg4);
+        }
+
+        public void Purge()
+        {
+            Delegate[] delegates = Delegate.GetInvocationList();
+            foreach (Delegate del in delegates)
+            {
+                Delegate -= (del as Action<T1, T2, T3, T4>);
+            }
+            Delegate = null;
+        }
     }
 }
