daac83e18f619290954ac02f4f3ad14d0f03baca
Matteo Beltrame <bltmtt@gmail.com>
2021-03-18 19:22:16 +0100

    AI

---
+++ Runtime/Scripts/AI/Memory.cs
@@ -0,0 +1,95 @@
+﻿//Copyright (c) matteo
+//Memory.cs - com.tratteo.gibframe
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using GibFrame.Extensions;
+
+namespace GibFrame.AI
+{
+    public class Memory
+    {
+        private List<MemoryModule> memories;
+        private bool inhibit = false;
+
+        public Memory()
+        {
+            memories = new List<MemoryModule>();
+            Thread forgetThread = new Thread(() => Forget_T());
+            forgetThread.Start();
+        }
+
+        ~Memory()
+        {
+            inhibit = true;
+        }
+
+        public void Remember(params MemoryModule[] modules)
+        {
+            foreach (MemoryModule module in modules)
+            {
+                MemoryModule mod;
+                if ((mod = Get(module.Memory)) != null)
+                {
+                    mod.Reset();
+                }
+                else
+                {
+                    memories.Add(module);
+                }
+            }
+        }
+
+        public bool Knows(object obj)
+        {
+            return memories.FindAll((m) => m.Memory.Equals(obj)).Count > 0;
+        }
+
+        public List<T> GetAllMemories<T>(Predicate<MemoryModule> predicate, Converter<MemoryModule, T> mapper)
+        {
+            return memories.GetPredicatesMatchingObjects(predicate).ConvertAll<T>(mapper);
+        }
+
+        public MemoryModule Get(object value)
+        {
+            return memories.Find((m) => m.Memory.Equals(value));
+        }
+
+        public bool Forget(object memory)
+        {
+            return ForgetAll((m) => m.Memory.Equals(memory)) > 0;
+        }
+
+        public int ForgetAll(Predicate<MemoryModule> Predicate)
+        {
+            return memories.RemoveAll(Predicate);
+        }
+
+        public bool Forget(MemoryModule module)
+        {
+            return memories.Remove(module);
+        }
+
+        public void Amnesia()
+        {
+            memories.Clear();
+        }
+
+        private void Forget_T()
+        {
+            while (!inhibit)
+            {
+                foreach (MemoryModule module in memories)
+                {
+                    if (module != null)
+                    {
+                        module.TimeStep(0.05F);
+                    }
+                }
+                ForgetAll((m) => m.RemembranceTime <= 0);
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
---
+++ Runtime/Scripts/AI/MemoryModule.cs
@@ -0,0 +1,45 @@
+﻿//Copyright (c) matteo
+//MemoryModule.cs - com.tratteo.gibframe
+
+using System;
+
+namespace GibFrame.AI
+{
+    public class MemoryModule : IEquatable<MemoryModule>
+    {
+        private float remembranceTime;
+
+        public float RemembranceTime { get => remembranceTime; }
+
+        public float InitialRemembranceTime { get; private set; }
+
+        public object Memory { get; private set; }
+
+        public MemoryModule(object memory, float remembranceTime)
+        {
+            this.remembranceTime = remembranceTime;
+            InitialRemembranceTime = remembranceTime;
+            Memory = memory;
+        }
+
+        public void Reset(float remembranceTime = -1)
+        {
+            this.remembranceTime = remembranceTime == -1 ? InitialRemembranceTime : remembranceTime;
+        }
+
+        public bool Equals(MemoryModule other)
+        {
+            return Memory.Equals(other.Memory);
+        }
+
+        public void TimeStep(float step)
+        {
+            remembranceTime -= step;
+        }
+
+        public override string ToString()
+        {
+            return Memory.ToString() + ", RT: " + InitialRemembranceTime;
+        }
+    }
+}
